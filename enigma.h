a6444e328b5cb4efdcf9746521ae24a8b46342d9
Ethan DyTioco <ethandytioco@gmail.com>
2020-12-22 10:15:04 -0500

    prep for steckerboard

--- enigma.h
+++ enigma.h
@@ -8,15 +8,35 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <unordered_map>
+#include "rotor.h"
 using namespace std;
 
 class Enigma{
 private:
-	
+	unordered_map<int, Rotor> rotorBank;
+	Rotor reflector, left, center, right, plugboard;
+	char ringsetting_l, ringsetting_c, ringsetting_r;
+	char groundsetting_l, groundsetting_c, groundsetting_r;
 	
 public:
 	Enigma();
-	void start();
+
+	void menu();
+	
+	void configure();
+	void ringstellung(char l, char c, char r);
+	void grundstellung(char l, char c, char r);
+	void steckerbrett(string pairs);
+	
+	void viewConfigurationLite();
+	void viewConfiguration();
+	
+	void useEnigma();
+	string scrubInput(string input);
+	void rotation();
+	int intForceWithinRange(int n);
+	void diagnostic();
 };
 
 #endif
